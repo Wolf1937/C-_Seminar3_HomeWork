2934561c75c4d350ccec6611b14090f7e9eafdff
Krutinis <krutinis@yandex.ru>
2022-10-10 12:55:54 +0300

    add Zadaca_23

---
+++ Zadacha_23/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+Console.WriteLine("Введите число N больше 1: ");
+int N = int.Parse(Console.ReadLine()!);
+int count = 1;
+double res = 1;
+if (N>0) {
+    while (count<=N) {
+        res = Math.Pow(count,3);
+        Console.Write($"{res}, ");
+        count++;
+    }
+Console.WriteLine();
+}
+else {  
+    Console.WriteLine("Некоректное значение!");
+}
